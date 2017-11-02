578ebdfe6031c1f25bd2d753c8259d3bc5d593a5
Matheus Felix <matheusgfelix@gmail.com>
2017-11-02 20:18:43 -0200

    Added purchase model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102203109) do
+ActiveRecord::Schema.define(version: 20171102221503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "purchaser_name"
+    t.text     "item_description"
+    t.decimal  "item_price"
+    t.integer  "purchase_count"
+    t.text     "merchant_address"
+    t.string   "merchant_name"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
